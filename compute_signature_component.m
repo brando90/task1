82289caecfdc784b121a94d52f4df8878ec1caad
Brando Miranda <brandomiranda@Brandos-MacBook-Pro-2.local>
2015-05-25 15:19:56 -0400

    generate signature vector

--- compute_signature_component.m
+++ compute_signature_component.m
@@ -2,14 +2,10 @@ function [ mu_k_n ] = compute_signature_component( I, t_k, n, G )
 %compute_signature_component summary = computes mu^k_n(I)
 %   computes signature
 
-[g_m g_n g_o] = size(G);
+[~, ~, g_o] = size(G);
 for i = 1:g_o;
     g = G(:,:, i);
-    proj = dot(I, inv(g)t)
-    sigmf(proj, [1 0]);
-    
-
-
-
+    proj = dot(I, g \ t_k);
+    mu_k_n = sigmf(proj, [1 0]) / g_o;
 end
 
--- creating_signature_circular_shift.m
+++ creating_signature_circular_shift.m
@@ -1,10 +1,21 @@
+%dimension space
+d = 3;
+
+%generate random image I
+I = rando([-1000, 1000], d, 1);
+
+%group circular shift
 circ = [0 0 1; 1 0 0; 0 1 0];
 G(: , : , 1) = circ;
 
 %number of tempaltes
-K = 10;
+K = 4;
+%number of components in a signature
+N = 12;
+
 
 %generate random templates K
+templates(:, K) = zeros(d, K);
 for k = 1:K;
     r = rando([-1000, 1000], 3, 1); %3 x 1 random vector
     r = r / norm(r, 2);
@@ -12,6 +23,17 @@ for k = 1:K;
 end
 
 [t_m, t_n] = size(templates);
-for t = 1:t_n;
-    
+for k = 1:t_n;
+    t_k = templates(:, k);
+    %compute signature for template t_k
+    mu_k = zeros(N, 1);
+    for n = 1:N;
+        mu_k_n = compute_signature_component(I, t_k, n, G);
+        mu_k(n,1) = mu_k_n;
+    end
 end
+
+
+
+%compute distance between signatures:
+
