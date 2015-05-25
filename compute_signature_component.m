cb3c41bf2521700f95a754da45f3de50154223c4
Brando Miranda <brandomiranda@Brandos-MacBook-Pro-2.local>
2015-05-25 19:34:29 -0400

    generalized version works

--- compute_signature_component.m
+++ compute_signature_component.m
@@ -2,12 +2,10 @@ function [ mu_k_n ] = compute_signature_component(I, t_k, n, G )
 %compute_signature_component summary = computes mu^k_n(I)
 %   computes signature
 
-[~, ~, g_o] = size(G);
-for i = 1:g_o;
-    %g = G(:,:, i);
-    g = G;
-    %proj = dot(I, g \ t_k);
-    proj = dot(I, inv(g) *t_k);
-    mu_k_n = sigmf(proj, [1 0]) / g_o;
+[~, ~, size_of_group] = size(G);
+for i = 1:size_of_group;
+    g = G(:,:, i);
+    proj = dot(I, g \ t_k);
+    mu_k_n = sigmf(proj, [1 0]) / size_of_group;
 end
 
--- creating_signature_circular_shift_generalized_dim1.m
+++ creating_signature_circular_shift_generalized_dim1.m
@@ -1,41 +1,28 @@
 %dimension space
-d = 3;
+d = 10;
 
 %generate random image I
-I = randi([-1000, 1000], d, 1);
-I = I / norm( I, 2)^2;
-%I = [1 ; 2; 3];
+I = generate_random_image(d, -1000, 1000);
 
 %group circular shift
-G = [0 0 1; 1 0 0; 0 1 0];
-circ = G;
+G = generate_circular_shift(d);
 
 %number of tempaltes
-K = 4;
+K = 5;
 %number of components in a signature
-N = 12;
+N = 3;
 
 
 %generate random templates K
 templates = zeros(d, K);
 for k = 1:K;
-    r = randi([-1000, 1000], 3, 1); %3 x 1 random vector
+    r = randi([-1000, 1000], d, 1); %3 x 1 random vector
     r = r / norm(r, 2)^2;
     templates(:, k) = r;
 end
 
-% t1 = [4; 2; 9];
-% t2 = [5; 5; 5];
-% t3 = [7; 8; 9];
-% t4 = [1; 5; 11];
-% templates(:, 1) = t1;
-% templates(:, 2) = t2;
-% templates(:, 3) = t3;
-% templates(:, 4) = t4;
-
-[t_m, t_n] = size(templates);
 sigma_I = generate_signature(I, templates, G, N);
-gI = circ * I;
+gI = G(1) * I;
 sigma_gI = generate_signature(gI, templates, G, N);
 
 %compute distance between signatures:
